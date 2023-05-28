bafb2e28f7cce5217b04062ba7ba544d27b68625
AsiaE14 <117573680+AsiaE14@users.noreply.github.com>
2023-05-28 12:38:52 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("Hello!")
+basic.showIcon(IconNames.Surprised)
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.showIcon(IconNames.Yes)
+    basic.showNumber(5)
+    basic.showIcon(IconNames.No)
+})
